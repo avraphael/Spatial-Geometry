168e76545169873205542a41767d63afcfdb5ded
Tony <avraphael4623@eagle.fgcu.edu>
2026-02-22 18:38:14 -0500

    Created a visualization with slider control of curvature

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import geoopt
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -43,7 +44,7 @@ def setup_plot(manifold, lo=None, width=7, height=7, grid_line_width=0.3, with_b
 
 
     # add background color
-    if K > 0 and with_background:
+    if not(K < 0) and with_background:
         plt.gca().set_facecolor(COLORS.BACKGROUND_BLUE)
 
     # set up plot axes and aspect ratio
@@ -58,30 +59,12 @@ def setup_plot(manifold, lo=None, width=7, height=7, grid_line_width=0.3, with_b
     plt.gca().set_aspect("equal")
 
     # add grid of geodesics
-    add_geodesic_grid(plt, manifold, lo, hi, line_width=grid_line_width)
+    add_geodesic_grid(plt.gca(), manifold, line_width=grid_line_width)
 
     return fig, plt, (lo, hi)
 
 
-def get_maximal_numerical_distance(manifold):
-    # get manifold properties
-    K = manifold.k
-    R = manifold.radius
-    # determine maximal distance from origin
-    dist0 = None
-    if K < 0:
-        # create point on R
-        r = torch.tensor((R, 0.0), dtype=torch.float64)
-        # project point on R into valid range (epsilon border)
-        r = manifold.projx(r)
-        # determine distance from origin
-        dist0 = manifold.dist0(r).item()
-    else:
-        dist0 = math.pi * R
-    return dist0
-
-
-def add_geodesic_grid(plt, manifold, lo, hi, line_width = 0.1):
+def add_geodesic_grid(ax: plt.Axes, manifold: geoopt.Stereographic, line_width=0.1):
 
     # define geodesic grid parameters
     N_EVALS_PER_GEODESIC = 10000
@@ -94,10 +77,17 @@ def add_geodesic_grid(plt, manifold, lo, hi, line_width = 0.1):
     R = manifold.radius.item()
 
     # get maximal numerical distance to origin on manifold
-    max_dist_0 = get_maximal_numerical_distance(manifold)
-
+    if K < 0:
+        # create point on R
+        r = torch.tensor((R, 0.0), dtype=manifold.dtype)
+        # project point on R into valid range (epsilon border)
+        r = manifold.projx(r)
+        # determine distance from origin
+        max_dist_0 = manifold.dist0(r).item()
+    else:
+        max_dist_0 = np.pi * R
     # adjust line interval for spherical geometry
-    circumference = 2*math.pi*R
+    circumference = 2*np.pi*R
 
     # determine reasonable number of geodesics
     # choose the grid interval size always as if we'd be in spherical
@@ -110,7 +100,6 @@ def add_geodesic_grid(plt, manifold, lo, hi, line_width = 0.1):
         n_geodesics_per_quadrant = int(max_dist_0 / grid_interval_size)
 
     # create time evaluation array for geodesics
-    min_t = 0.0
     if K < 0:
         min_t = -1.2*max_dist_0
     else:
@@ -118,8 +107,8 @@ def add_geodesic_grid(plt, manifold, lo, hi, line_width = 0.1):
     t = torch.linspace(min_t, -min_t, N_EVALS_PER_GEODESIC)[:, None]
 
     # define a function to plot the geodesics
-    def plot_geodesic(gv, **kwargs):
-        plt.plot(*gv.t().numpy(), STYLE, color=COLOR, linewidth=LINE_WIDTH)
+    def plot_geodesic(gv):
+        ax.plot(*gv.t().numpy(), STYLE, color=COLOR, linewidth=LINE_WIDTH)
 
     # define geodesic directions
     u_x = torch.tensor((0.0, 1.0))
@@ -136,15 +125,15 @@ def add_geodesic_grid(plt, manifold, lo, hi, line_width = 0.1):
         # add the crosshair manually for the sproj of sphere
         # because the lines tend to get thicker if plotted
         # as done for K<0
-        plt.plot([lo, hi], [0, 0], STYLE, color=COLOR, linewidth=LINE_WIDTH)
-        plt.plot([0, 0], [lo, hi], STYLE, color=COLOR, linewidth=LINE_WIDTH)
+        ax.axvline(0, linestyle=STYLE, color=COLOR, linewidth=LINE_WIDTH)
+        ax.axhline(0, linestyle=STYLE, color=COLOR, linewidth=LINE_WIDTH)
 
     # add geodesics per quadrant
     for i in range(1, n_geodesics_per_quadrant):
-
+        i = torch.as_tensor(float(i))
         # determine start of geodesic on x/y-crosshair
-        x = manifold.geodesic_unit(torch.tensor(i*grid_interval_size), o, u_y)
-        y = manifold.geodesic_unit(torch.tensor(i*grid_interval_size), o, u_x)
+        x = manifold.geodesic_unit(i*grid_interval_size, o, u_y)
+        y = manifold.geodesic_unit(i*grid_interval_size, o, u_x)
 
         # compute point on geodesics
         x_geodesic = manifold.geodesic_unit(t, x, u_x)
